2725836b320405e6c5ad010f6161b4a0e0ecc00e
Alicia Prather <Alprather@gmail.com>
2016-01-26 09:04:00 +0400

    Seeded sponsored posts, ready to complete controllers

--- db/seeds.rb
+++ db/seeds.rb
@@ -23,8 +23,17 @@ posts = Post.all
     body: RandomData.random_paragraph
   )
 end
+100.times do
+  SponsoredPost.create!(
+    topic: topics.sample,
+    title: RandomData.random_sentence,
+    body: RandomData.random_paragraph,
+    price: 99
+  )
+end
 
 puts "Seed finished"
 puts "#{Topic.count} topics created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
+puts "#{SponsoredPost.count} sponsored posts created"
