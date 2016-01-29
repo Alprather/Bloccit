79122113f8fb20c0b77b4abb624b30b2d4ecdf07
Alicia Prather <Alprather@gmail.com>
2016-01-29 13:17:54 +0400

    checkpoint-39 complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,13 @@
+ # Create Users
+5.times do
+  User.create!(
+  name:     RandomData.random_name,
+  email:    RandomData.random_email,
+  password: RandomData.random_sentence
+  )
+end
+users = User.all
+
 # Create Posts
 15.times do
   Topic.create!(
@@ -9,6 +19,7 @@ topics = Topic.all
 
 50.times do
   Post.create!(
+    user:   users.sample,
     topic:  topics.sample,
     title:  RandomData.random_sentence,
     body:   RandomData.random_paragraph
@@ -23,8 +34,13 @@ posts = Post.all
     body: RandomData.random_paragraph
   )
 end
-
+user = User.first
+user.update_attributes!(
+email: 'alprather@gmail.com', # replace this with your personal email
+password: 'helloworld'
+)
 puts "Seed finished"
+ puts "#{User.count} users created"
 puts "#{Topic.count} topics created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
