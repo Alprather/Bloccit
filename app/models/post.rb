d2972d0001634bbb6b091d5acecf33651fc885ec
Alicia Prather <Alprather@gmail.com>
2016-01-30 18:42:09 +0400

    assignment-43 complete

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,6 +5,7 @@ belongs_to :user
    has_many :labelings, as: :labelable
    has_many :labels, through: :labelings
    has_many :votes, dependent: :destroy
+   after_create :create_vote
 default_scope { order('rank DESC') }
 
   validates :title, length: { minimum: 5 }, presence: true
@@ -26,5 +27,9 @@ def update_rank
    age_in_days = (created_at - Time.new(1970,1,1)) / 1.day.seconds
    new_rank = points + age_in_days
    update_attribute(:rank, new_rank)
+ end
+ private 
+ def create_vote
+   user.votes.create(value: 1, post: self)
  end
 end
