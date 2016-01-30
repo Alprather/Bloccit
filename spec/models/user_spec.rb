ce9396e492db30a0628c9e107e6c8c45ad6a9515
Alicia Prather <Alprather@gmail.com>
2016-01-30 11:11:46 +0400

    checkpoint 41 complete

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   let(:user) { User.create!(name: "Bloccit User", email: "user@bloccit.com", password: "password") }
   it { should have_many(:posts)}
+   it { should have_many(:comments) }
    # Shoulda tests for name
    it { should validate_presence_of(:name) }
    it { should validate_length_of(:name).is_at_least(1) }
@@ -70,7 +71,7 @@ describe "roles" do
     end
   end
 end
-  
+
    describe "invalid user" do
   let(:user_with_invalid_name) { User.new(name: "", email: "user@bloccit.com") }
   let(:user_with_invalid_email) { User.new(name: "Bloccit User", email: "") }
