dbc28ad3f9665f58b2165d84ba1ad2893c3a05ec
Alicia Prather <Alprather@gmail.com>
2016-01-31 14:15:12 +0400

    assignment 44 complete

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,6 +6,7 @@ belongs_to :user
    has_many :labels, through: :labelings
    has_many :votes, dependent: :destroy
    has_many :favorites, dependent: :destroy
+   after_create :create_favorite
 default_scope { order('rank DESC') }
 
   validates :title, length: { minimum: 5 }, presence: true
@@ -27,5 +28,9 @@ def update_rank
    age_in_days = (created_at - Time.new(1970,1,1)) / 1.day.seconds
    new_rank = points + age_in_days
    update_attribute(:rank, new_rank)
+ end
+ def create_favorite
+   Favorite.create(post: self, user: self.user)
+   FavoriteMailer.new_post(self).deliver_now
  end
 end
