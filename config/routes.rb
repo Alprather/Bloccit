143c5f43f5a58ed5be6a7ebb0c5c576407e96265
Alicia Prather <Alprather@gmail.com>
2016-02-01 17:23:50 +0400

    checkpoint 45 complete

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 resources :labels, only: [:show]
 
  resources :topics do
-  resources :posts, except: [:index]
+resources :posts, except: [:index]
 end
 resources :posts, only: [] do
    resources :comments, only: [:create, :destroy]
@@ -11,7 +11,7 @@ resources :posts, only: [] do
    post '/up-vote' => 'votes#up_vote', as: :up_vote
    post '/down-vote' => 'votes#down_vote', as: :down_vote
  end
-resources :users, only: [:new, :create]
+resources :users, only: [:new, :create, :show]
 resources :sessions, only: [:new, :create, :destroy]
 post 'users/confirm' => 'users#confirm'
    get 'about' => 'welcome#about'
